bac60914e7e414a61e15ab8c4d13c9b76eb149f0
tonyvazgar <luis.vazquezga@udlap.mx>
2016-10-13 21:51:48 -0500

    Code for tower of hanoi in Prolog.
    This code gives you a list of lists with all the movements that you do when you play it.

--- hanoiDiscos.pl
+++
@@ -1,3 +0,0 @@
-hanoi(N, Solucion):-mover(N, 1, 3, 2, Solucion).
-mover(0, _, _, _, []):-!.
-mover(N, Origen, Paso, Destino, Solucion):-N1 is N-1,mover(N1, Origen, Destino, Paso, Solucion1),Original is N1 + 1,append(Solucion1,[[Original,Paso]], Solucion2),mover(N1, Destino, Paso, Origen, Solucion3),append(Solucion2, Solucion3, Solucion).
